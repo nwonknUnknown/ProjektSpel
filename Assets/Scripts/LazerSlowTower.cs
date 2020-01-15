a6a9b361c1ab2c315f3d82cc4083d6eaf3255bb2
sssebastiannn <59011241+sssebastiannn@users.noreply.github.com>
2020-01-15 03:50:15 +0100

    Fixade alla torn. de skjuter inte längre 2ggr

--- Assets/Scripts/LazerSlowTower.cs
+++ Assets/Scripts/LazerSlowTower.cs
@@ -7,29 +7,14 @@ public class LazerSlowTower : TurretActions
     [SerializeField] LineRenderer linerenderer;
     [SerializeField] float damageOT;
     [SerializeField] float slowDown;
-    public ParticleSystem enemyHitEffect;
+    [SerializeField]ParticleSystem enemyHitEffect;
 
     internal override void Update()
     {
-        if (!HpManager.instance.CheckIfLost())
-        {
-            if (target == null)
-            {
-
-                if (linerenderer.enabled)
-                {
-
-                    linerenderer.enabled = false;
-                    enemyHitEffect.Stop();
-                }
-                   
-                return;
-            }
-        }
-
-        if (!LockOnTarget())
+        if (!LockOnTarget() && !hasTarget)
         {
             linerenderer.enabled = false;
+            enemyHitEffect.Stop();
         }
 
         Shoot();
@@ -37,23 +22,18 @@ public class LazerSlowTower : TurretActions
     internal override void Shoot()
     {
 
-        target.GetComponent<EnemyStats>().RemoveHP(damageOT * Time.deltaTime);
-        target.GetComponent<EnemyMovement>().Slow(slowDown);
-
-        if (!linerenderer.enabled)
+        if (!linerenderer.enabled && hasTarget)
         {
             linerenderer.enabled = true;
             enemyHitEffect.Play();
         }
-            
 
+        target.GetComponent<EnemyStats>().RemoveHP(damageOT * Time.deltaTime);
+        target.GetComponent<EnemyMovement>().Slow(slowDown);
         linerenderer.SetPosition(0, bulletSpawnposition.position);
         linerenderer.SetPosition(1, target.transform.position);
-
         Vector3 dir = bulletSpawnposition.position - target.transform.position;
-
         enemyHitEffect.transform.position = target.transform.position + dir.normalized * .5f;
-        
         enemyHitEffect.transform.rotation = Quaternion.LookRotation(dir);
 
     }
--- Assets/Scripts/TurretActions.cs
+++ Assets/Scripts/TurretActions.cs
@@ -11,22 +11,20 @@ public class TurretActions : MonoBehaviour
     [SerializeField] internal Transform bulletSpawnrotation;
     [SerializeField] private Projectile[] bulletPrefab;//
 
-
     [SerializeField] private float timeBetweenShots = 0f;
     [SerializeField] private int turretDamage = 1;
     [SerializeField] private float turnspeed = 10f;
-    private float time = 0;
 
+    public float time = 0;
     public int startAmmo = 10;
     public int currentAmmo;
     public int bulletCost = 0;
     public int turretCost;
     int curBulletPrefab = 0;//
 
-    private int targetsInTrigger;// UNUSED BOOL?
-    private bool hasTarget = false;
+    internal bool hasTarget = false;
 
-    public void Start()
+    void Start()
     {
         time = timeBetweenShots;
         currentAmmo = startAmmo;
@@ -43,19 +41,6 @@ public class TurretActions : MonoBehaviour
                 return;
             }
 
-            if (hasTarget && currentAmmo > 0 && target.GetComponent<EnemyStats>().hp > 0)
-            {
-                Vector3 dir = target.transform.position - transform.position;
-                Quaternion lookRotation = Quaternion.LookRotation(dir);
-                Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnspeed).eulerAngles;
-                transform.rotation = Quaternion.Euler(0, rotation.y, 0);
-
-            }
-            else if (currentAmmo <= 0)
-            {
-                RequestAmmo();
-            }
-    
         LockOnTarget();
 
         Shoot();
@@ -85,13 +70,16 @@ public class TurretActions : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
+
         if (other.gameObject == target)
         {
             hasTarget = false;
+                
         }
+
     }
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (!hasTarget)
         {
@@ -103,7 +91,7 @@ public class TurretActions : MonoBehaviour
     virtual internal void Shoot()
     {
         time += Time.deltaTime;
-        if (time >= timeBetweenShots && currentAmmo > 0)
+        if (time >= timeBetweenShots && currentAmmo > 0 && hasTarget)
         {
             if (GetComponentInChildren<PlayMyAnimation>())
             {
@@ -160,156 +148,4 @@ public class TurretActions : MonoBehaviour
         return allstats;
     }
 }
-
-/*using UnityEngine;
-
-//Simon Voss, Alexander Kourie
-//Handles all actions of the turret, including: Targeting, Asking for ammo, Shooting bullets, deciding the bullets damage, and updating text of refill-cost of this turrets UI-panels
-
-public class TurretActions : MonoBehaviour
-{
-    internal GameObject target;
-
-    [SerializeField] internal Transform bulletSpawnposition;
-    [SerializeField] internal Transform bulletSpawnrotation;
-    [SerializeField] public Projectile bullet;
-
-    [SerializeField] private float timeBetweenShots = 1f;
-    [SerializeField] private int turretDamage = 1;
-    [SerializeField] private float turnspeed = 10f;
-    private float time = 0;
-
-    public int startAmmo = 10;
-    public int currentAmmo;
-    public int bulletCost = 0;
-    public int turretCost;
-
-    private int targetsInTrigger;// UNUSED BOOL?
-    private bool hasTarget = false;
-
-    public void Start()
-    {
-        currentAmmo = startAmmo;
-        transform.parent.Find("TurretUI").gameObject.SetActive(true);
-        transform.parent.Find("TurretUI").Find("Choices").Find("CurrentAmmo").GetComponentInChildren<UpdateMyText>().UpdateMyTextAsInt(currentAmmo);
-        transform.parent.Find("TurretUI").Find("Choices").Find("RefillAmmo").GetComponentInChildren<UpdateMyText>().UpdateRefillCostAsInt((startAmmo - currentAmmo) * bulletCost);
-        transform.parent.Find("TurretUI").gameObject.SetActive(false);
-    }
-
-    internal virtual void Update()
-    {
-        //if (!HpManager.instance.CheckIfLost())
-        //{
-            if (target == null)
-            {
-                return;
-            }
-
-            if (hasTarget && currentAmmo > 0 && target.GetComponent<EnemyStats>().hp > 0)
-            {
-                Vector3 dir = target.transform.position - transform.position;
-                Quaternion lookRotation = Quaternion.LookRotation(dir);
-                Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnspeed).eulerAngles;
-                transform.rotation = Quaternion.Euler(0, rotation.y, 0);
-                Shoot();
-            }
-            else if (currentAmmo <= 0)
-            {
-                RequestAmmo();
-            }
-        //}
-
-        LockOnTarget();
-
-        Shoot();
-    }
-
-    internal bool LockOnTarget()
-    {
-        if (hasTarget && currentAmmo > 0 && target.GetComponent<EnemyStats>().hp > 0)
-        {
-            Vector3 dir = target.transform.position - transform.position;
-            Quaternion lookRotation = Quaternion.LookRotation(dir);
-            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnspeed).eulerAngles;
-            transform.rotation = Quaternion.Euler(0, rotation.y, 0);
-            return true;
-
-        }
-        else if (currentAmmo <= 0)
-        {
-            RequestAmmo();
-            return false;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == target)
-        {
-            hasTarget = false;
-        }
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if (!hasTarget)
-        {
-            target = other.gameObject;
-            hasTarget = true;
-        }
-    }
-
-    virtual internal void Shoot()
-    {
-        time += Time.deltaTime;
-        if (time >= timeBetweenShots && currentAmmo > 0)
-        {
-            GetComponentInChildren<PlayMyAnimation>().PlayShoot();
-            GetComponent<Soundcontroller>().PlaySound();
-            Projectile thisBullet = Instantiate(bullet, bulletSpawnposition.position, bulletSpawnrotation.rotation);
-            thisBullet.AssignTarget(target);
-            thisBullet.AssignTurret(gameObject);
-            thisBullet.Damage = turretDamage;
-            currentAmmo--;
-            if (transform.parent.Find("TurretUI").gameObject.activeSelf)
-            {
-                transform.parent.Find("TurretUI").Find("Choices").Find("CurrentAmmo").GetComponentInChildren<UpdateMyText>().UpdateMyTextAsInt(currentAmmo);
-                transform.parent.Find("TurretUI").Find("Choices").Find("RefillAmmo").GetComponentInChildren<UpdateMyText>().UpdateRefillCostAsInt((startAmmo - currentAmmo) * bulletCost);
-            }
-            else
-            {
-                transform.parent.Find("TurretUI").gameObject.SetActive(true);
-                transform.parent.Find("TurretUI").Find("Choices").Find("CurrentAmmo").GetComponentInChildren<UpdateMyText>().UpdateMyTextAsInt(currentAmmo);
-                transform.parent.Find("TurretUI").Find("Choices").Find("RefillAmmo").GetComponentInChildren<UpdateMyText>().UpdateRefillCostAsInt((startAmmo - currentAmmo) * bulletCost);
-                transform.parent.Find("TurretUI").gameObject.SetActive(false);
-            }
-
-            time -= timeBetweenShots;
-
-        }
-    }
-
-    void RequestAmmo()
-    {
-        transform.parent.Find("NoAmmoUI").gameObject.SetActive(true);
-        transform.parent.Find("TurretUI").gameObject.SetActive(false);
-        transform.parent.Find("NoAmmoUI").GetComponentInChildren<UpdateMyText>().UpdateRefillCostAsInt((startAmmo - currentAmmo) * bulletCost);
-    }
-
-    public string GetTurretStats()
-    {
-        string allstats;
-
-        allstats = "\n" + "Damage: " + turretDamage.ToString();
-        allstats += "\n" + "Firerate: " + (1 / timeBetweenShots).ToString() + " shot(s)/s";
-        allstats += "\n" + "Cost: " + turretCost.ToString() + " Gold";
-        allstats += "\n" + "Ammo: " + startAmmo.ToString();
-        allstats += "\n" + "Refill cost/bullet: " + bulletCost;
-        return allstats;
-    }
-}*/
 
